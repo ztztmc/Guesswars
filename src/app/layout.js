8c9437c6855c561f23c811661bc3b14894435e6c
ztzt <113851739+ztztmc@users.noreply.github.com>
2025-09-20 17:08:49 +0530

    Add vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Providers } from "./providers";
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}
       >
-        <Providers>{children}</Providers>
+        <Providers>{children}<Analytics /></Providers>
       </body>
     </html>
   );
